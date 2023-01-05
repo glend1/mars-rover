e4179beed8a16ab52a81f51d65297636f500847d
glen <glen_dovey@hotmail.com>
2023-01-05 19:05:14 +0000

    added rotate left/right functions

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -5,6 +5,36 @@ describe("createPlateau", () => {
       expect(utils.createPlateau(1)).toStrictEqual([[""]]);
     });
     it("Should return an array of 5 length", () => {
-      expect(utils.createPlateau(5)).toStrictEqual([["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""]]);
+        expect(utils.createPlateau(5)).toStrictEqual([["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""]]);
     });
-  });
+});
+
+describe("rotateLeft", () => {
+      it("Should change N to W", () => {
+        expect(utils.rotateLeft("N")).toStrictEqual("W");
+      });
+      it("Should change W to S", () => {
+        expect(utils.rotateLeft("W")).toStrictEqual("S");
+      });
+      it("Should change S to E", () => {
+        expect(utils.rotateLeft("S")).toStrictEqual("E");
+      });
+      it("Should change E to N", () => {
+        expect(utils.rotateLeft("E")).toStrictEqual("N");
+      });
+  })
+
+describe("rotateRight", () => {
+      it("Should change N to E", () => {
+        expect(utils.rotateRight("N")).toStrictEqual("E");
+      });
+      it("Should change E to S", () => {
+        expect(utils.rotateRight("E")).toStrictEqual("S");
+      });
+      it("Should change S to W", () => {
+        expect(utils.rotateRight("S")).toStrictEqual("W");
+      });
+      it("Should change W to N", () => {
+        expect(utils.rotateRight("W")).toStrictEqual("N");
+      });
+  })
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,17 @@
 export const createPlateau = (size: number): string[][] => {
     return Array(size).fill(Array(size).fill(""))
 }
+
+type directions = "N" | "E" | "S" | "W"
+
+const directions = ["N", "E", "S", "W"]
+
+export const rotateLeft = (direction: directions) => {
+    if (direction === "N") return directions[3]
+    return directions[directions.indexOf(direction) - 1]
+}
+
+export const rotateRight = (direction: directions) => {
+    if (direction === "W") return directions[0]
+    return directions[directions.indexOf(direction) + 1]
+}
